84859b2cbfd2d87f26f2d48caf1249c549a3777d
Adi Gosto <adi.gosto@edu.fit.ba>
2024-11-06 13:10:44 +0100

    Linija

---
+++ backend/RS1_2024_25.API/Data/Models/Line.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Line
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StartingStation { get; set; }
+        public string EndingStation { get; set; }
+        public string CompleteDistance { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
