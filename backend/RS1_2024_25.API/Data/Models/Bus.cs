df34cd79499c0610d582ed7d2fc38356a22ea719
Nedim <nedim.jugo@edu.fit.ba>
2024-11-06 11:50:57 +0100

    Busses

---
+++ backend/RS1_2024_25.API/Data/Models/Bus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Bus
+    {
+        [Key]
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Manufacturer {  get; set; }
+        public string Model { get; set; } 
+        public string Capacity { get; set; }
+        public string ManufactureYear { get; set; }
+    }
+}
