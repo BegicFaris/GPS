f7908e3678aa2cf4f29d12d3a45b2b43b0ff9e13
Adi Gosto <adigosto88@gmail.com>
2024-11-13 21:09:00 +0100

    Routes and Credit Cards CRUD backend part/some more landing page sketches on the frontend part

---
+++ backend/RS1_2024_25.API/Services/RouteServices/RouteService.cs
@@ -0,0 +1,42 @@
+﻿using RS1_2024_25.API.Data.Models;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace RS1_2024_25.API.Services.RouteServices
+{
+    public class RouteService : IRouteService
+    {
+        private readonly ApplicationDbContext _context;
+        public RouteService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Data.Models.Route>> GetAllRoutesAsync() =>
+            await _context.Routes.ToListAsync();
+        public async Task<Data.Models.Route> GetRouteByIdAsync(int id) =>
+          await _context.Routes.FindAsync(id);
+        public async Task<Data.Models.Route> CreateRouteAsync(Data.Models.Route route)
+        {
+            _context.Routes.Add(route);
+            await _context.SaveChangesAsync();
+            return route;
+        }
+
+        public async Task<Data.Models.Route> UpdateRouteAsync(Data.Models.Route route)
+        {
+            _context.Routes.Update(route);
+            await _context.SaveChangesAsync();
+            return route;
+        }
+
+        public async Task<bool> DeleteRouteAsync(int id)
+        {
+            var route = await _context.Routes.FindAsync(id);
+            if (route == null) return false;
+            _context.Routes.Remove(route);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
