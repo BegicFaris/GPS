b1d80225508d40965dacfa269d17a0b153c26217
Faris Begić <faris.begic@edu.fit.ba>
2024-11-19 22:01:33 +0000

    Deleted backend

--- backend/RS1_2024_25.API/Data/Models/IMustHaveTenant.cs
+++
@@ -1,9 +0,0 @@
-
-namespace RS1_2024_25.API.Data.Models
-{
-    //Interface that is used to differentiate classes that use multitenantcy
-    public interface IMustHaveTenant
-    {
-        public string TenantId { get; set; }
-    }
-}
--- backend/RS1_2024_25.API/Data/Models/NotificationType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RS1_2024_25.API.Data.Models
-{
-    public class NotificationType
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-    }
-}
