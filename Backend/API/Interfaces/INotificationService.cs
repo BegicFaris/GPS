56751be0374cb49dd7a70f15e10cb43d70c7e40e
Begic Faris <farisbegic9@gmail.com>
2024-11-21 14:08:08 +0100

    Notif Controller

---
+++ Backend/API/Interfaces/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using GPS.API.Data.Models;
+
+namespace GPS.API.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<Notification>> GetAllNotificationsAsync();
+        Task<Notification> GetNotificationByIdAsync(int id);
+        Task<Notification> CreateNotificationAsync(Notification notification);
+        Task<Notification> UpdateNotificationAsync(Notification notification  );
+        Task<bool> DeleteNotificationAsync(int id);
+    }
+}
---
+++ Backend/API/Services/NotificationService/NotificationService.cs
@@ -0,0 +1,44 @@
+﻿using GPS.API.Data.DbContexts;
+using GPS.API.Data.Models;
+using GPS.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GPS.API.Services.NotificationService
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly ApplicationDbContext _context;
+        public NotificationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Notification>> GetAllNotificationsAsync() =>
+            await _context.Notifications.ToListAsync();
+        public async Task<Notification> GetNotificationByIdAsync(int id) =>
+          await _context.Notifications.FindAsync(id);
+        public async Task<Notification> CreateNotificationAsync(Notification notification)
+        {
+            _context.Notifications.Add(notification);
+            await _context.SaveChangesAsync();
+            return notification;
+        }
+
+
+        public async Task<Notification> UpdateNotificationAsync(Notification notification)
+        {
+            _context.Notifications.Update(notification);
+            await _context.SaveChangesAsync();
+            return notification;
+        }
+
+        public async Task<bool> DeleteNotificationAsync(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification == null) return false;
+            _context.Notifications.Remove(notification);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
