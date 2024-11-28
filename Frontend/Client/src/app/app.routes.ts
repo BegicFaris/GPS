ef0b783e3226f7579fc4d8f6e9f56e3a9aa7bd71
Adi Gosto <adigosto88@gmail.com>
2024-11-28 15:13:13 +0100

    Test

--- Frontend/Client/src/app/app.routes.ts
+++ Frontend/Client/src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { BusListComponent } from './bus/bus-list/bus-list.component';
 import { BusFormComponent } from './bus/bus-form/bus-form.component';
 import { LineListComponent } from './line/line-list/line-list.component';
 import { LineFormComponent } from './line/line-form/line-form.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
-  { path: 'landing-page', component: LandingPageComponent },
   { path: 'buses', component: BusListComponent },
   { path: 'buses/add', component: BusFormComponent },
   { path: 'lines', component: LineListComponent },
   { path: 'lines/add', component: LineFormComponent },
+  { path: '', component: LandingPageComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
--- Frontend/Client/src/app/line/line-dto.model.ts
+++
@@ -1,9 +0,0 @@
-export interface LineDTO{
-    name: string;
-    startingStationId: number;
-    //startingStation: Station
-    endingStationId: number;
-    //endingStation: Station
-    completeDistance: string;
-    isActive: boolean;
-}
