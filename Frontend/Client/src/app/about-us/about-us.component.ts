ae48a4e1b9086633f1ca7e867f1b4538b0e15690
Adi Gosto <adi.gosto@edu.fit.ba>
2025-01-13 13:48:39 +0100

    Fixing some issues with google maps and footer

--- Frontend/Client/src/app/about-us/about-us.component.ts
+++ Frontend/Client/src/app/about-us/about-us.component.ts
@@ -18,8 +18,7 @@ export class AboutUsComponent {
     this.titleService.setTitle("About Us");
   }
   email: string = 'contact@mostarbus.com';
-  phone: string = '+387-123-4567';
+  phone: string = '+387/123-456-789';
   locationUrl: string =
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2871.6566638791314!2d17.814195815764468!3d43.343774479132024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134b16f13a4cb3bf%3A0x23a3e8f0d2d1bc1f!2sMostar%2C%20Bosnia%20and%20Herzegovina!5e0!3m2!1sen!2s!4v1693832822833!5m2!1sen!2s';
-
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2871.5962879511083!2d17.802233915764478!3d43.34421617913175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134b16ec1e5d325f%3A0x9f3e6fdf5dcf4e8!2s8R8H%2BRGH%2C%20Mostar%2088000%2C%20Bosnia%20and%20Herzegovina!5e0!3m2!1sen!2s!4v1701234567890!5m2!1sen!2s';
 }
