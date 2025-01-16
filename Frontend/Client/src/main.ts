8736b5acefd8047199b4df533f1f287c44e6aa37
Adi Gosto <adi.gosto@edu.fit.ba>
2025-01-16 22:40:39 +0100

    Implementing internationalization/mulitlanguage/i18n on About Us component and Nav component

--- Frontend/Client/src/main.ts
+++ Frontend/Client/src/main.ts
@@ -4,12 +4,32 @@ import { AppComponent } from './app/app.component';
 import { provideRouter, RouterModule } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './app/_services/auth-interceptor.service';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { importProvidersFrom } from '@angular/core';
+ 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/i18n/', '.json');
+}
+ 
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        },
+        defaultLanguage: 'en'
+      })
+    ),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes), 
     provideAnimations(),
