b7602377e18bb8e6a03c53415ebd0a4f06002653
Adi Gosto <adi.gosto@edu.fit.ba>
2025-01-16 23:34:32 +0100

    Fixing some small issues on Schedule and About Us components

--- Frontend/Client/src/app/_services/translate-http-lodader.service.ts
+++
@@ -1,7 +0,0 @@
-// src/app/_services/translate-http-loader.service.ts
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.ts');
-}
